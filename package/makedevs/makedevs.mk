28c67a02e6a92190922ceb232507d7cadee0b835
matson <matson.hall@gmail.com>
2012-09-20 07:07:39 -0400

    fix compile error

--- package/makedevs/makedevs.mk
+++ package/makedevs/makedevs.mk
@@ -12,7 +12,7 @@ $(MAKEDEVS_DIR)/makedevs.c:
 	cp package/makedevs/makedevs.c $(MAKEDEVS_DIR)
 
 $(MAKEDEVS_DIR)/makedevs: $(MAKEDEVS_DIR)/makedevs.c
-	$(CC) -Wall -Werror -O2 $(MAKEDEVS_DIR)/makedevs.c -o $@
+	$(CC) -Wall -O2 $(MAKEDEVS_DIR)/makedevs.c -o $@
 
 $(TARGET_DIR)/usr/bin/makedevs: $(MAKEDEVS_DIR)/makedevs
 	$(INSTALL) -m 755 $^ $@
@@ -28,7 +28,7 @@ $(HOST_MAKEDEVS_DIR)/makedevs.c:
 	cp package/makedevs/makedevs.c $(HOST_MAKEDEVS_DIR)
 
 $(HOST_MAKEDEVS_DIR)/makedevs: $(HOST_MAKEDEVS_DIR)/makedevs.c
-	$(CC) -Wall -Werror -O2 $(HOST_MAKEDEVS_DIR)/makedevs.c -o $@
+	$(CC) -Wall -O2 $(HOST_MAKEDEVS_DIR)/makedevs.c -o $@
 
 $(HOST_DIR)/usr/bin/makedevs: $(HOST_MAKEDEVS_DIR)/makedevs
 	$(INSTALL) -m 755 $^ $@
