b3c758066e97cedff052204f00b4606563542e89
Tom Cubie <Mr.hipboi@gmail.com>
2012-11-17 13:58:59 +0800

    use dhcp to config ip

--- target/skel/test/ir-runner/26.sh
+++ target/skel/test/ir-runner/26.sh
@@ -3,7 +3,8 @@
 MAC_ADDR="`uuidgen |awk -F- '{print $5}'|sed 's/../&:/g'|sed 's/\(.\)$//' |cut -b3-17`"
 ifconfig eth0 hw ether "48$MAC_ADDR"
 ifconfig lo 127.0.0.1
-ifconfig eth0 192.168.1.2/24
+#ifconfig eth0 192.168.1.2/24
+udhcpc -t 1 -n
 
 
 if ! /bin/ps |grep dwin |grep -v grep
